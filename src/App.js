388bc4764d1d4d66cec1a089f94a1c84538191b6
Cr7t3K <cvr.contact.pro@gmail.com>
2020-10-01 19:55:17 +0200

    React 03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-          avatar="https://randomuser.me/api/portraits/women/32.jpg"
-          name="Eleanor Silva"
-          online
-      />
-      <Contact
-          avatar="https://randomuser.me/api/portraits/women/52.jpg"
-          name="Linda Taylor"
-          online
-      />
-      <Contact
-          avatar="https://randomuser.me/api/portraits/men/89.jpg"
-          name="Rene Barnes"
-      />
+      <ContactList />
     </div>
   );
 }
